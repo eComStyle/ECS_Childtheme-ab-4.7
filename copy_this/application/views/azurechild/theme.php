5963a532b42dce9c5941882bad1633cd43c96fa4
nickname <info@ecomstyle.de>
2014-09-27 23:04:46 +0200

    Ready 4 OXID eShop 4.9

--- copy_this/application/views/azurechild/theme.php
+++ copy_this/application/views/azurechild/theme.php
@@ -18,11 +18,11 @@
  */
 $aTheme = array(
     
-    'title'        => 'Azure-Childtheme',
-    'description'  => 'Childtheme auf Basis des OXID Azurethemes. Themeeinstellungen werden im Azuretheme vorgenommen.',
-    'thumbnail'    => 'theme.jpg',
-    'version'      => '1.0',
-    'author'       => '<strong style="font-size: 17px;color:#04B431;">e</strong><strong style="font-size: 16px;">ComStyle.de</strong>',
-    'parentTheme'    => 'azure',
-    'parentVersions' => array('1.3', '1.3.1'),
+    'title'             => 'Azure-Childtheme',
+    'description'       => 'Childtheme auf Basis des OXID Azurethemes. Themeeinstellungen werden im Azuretheme vorgenommen.',
+    'thumbnail'         => 'theme.jpg',
+    'version'           => '1.0',
+    'author'            => '<a href="http://ecomstyle.de" target="_blank"><strong style="font-size: 17px;color:#04B431;">e</strong><strong style="font-size: 16px;">ComStyle.de</strong></a>',
+    'parentTheme'       => 'azure',
+    'parentVersions'    => array('1.3', '1.3.1', '1.4.0'),
 );
